5e34ba410d86b48d2c4dfc231d318bb679adbe3e
Majid Jabrayilov <cmecid@gmail.com>
2019-11-10 14:24:46 +0400

    Initial Commit

---
+++ Recipes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Recipes
+//
+//  Created by Majid Jabrayilov on 11/10/19.
+//  Copyright © 2019 Majid Jabrayilov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
