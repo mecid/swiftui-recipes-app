671be99f2d9ac05834796f6de925bde1fa3fc2d9
Majid Jabrayilov <cmecid@gmail.com>
2019-11-11 13:44:40 +0400

    Details screen implemented.

---
+++ Recipes/Features/Details/RecipeDetailsContainerView.swift
@@ -0,0 +1,50 @@
+//
+//  RecipeDetailsContainerView.swift
+//  Recipes
+//
+//  Created by Majid Jabrayilov on 11/10/19.
+//  Copyright © 2019 Majid Jabrayilov. All rights reserved.
+//
+import SwiftUI
+
+struct RecipeDetailsContainerView: View {
+    @EnvironmentObject var store: Store<AppState, AppAction>
+    @State private var webViewShown = false
+
+    let recipe: Recipe
+
+    private var isFavorited: Bool {
+        store.state.favorited.contains(recipe)
+    }
+
+    var body: some View {
+        RecipeDetailsView(recipe: recipe)
+            .navigationBarTitle(Text(recipe.title), displayMode: .inline)
+            .navigationBarItems(
+                trailing: HStack(spacing: 16) {
+                    Button(action: { self.webViewShown = true }) {
+                        Image(systemName: "list.number")
+                            .resizable()
+                            .frame(width: 24, height: 24)
+                    }
+
+                    Button(action: {
+                        if self.isFavorited {
+                            self.store.send(.removeFromFavorites(recipe: self.recipe))
+                        } else {
+                            self.store.send(.saveToFavorites(recipe: self.recipe))
+                        }
+                    }) {
+                        Image(systemName: isFavorited ? "heart.fill" : "heart")
+                            .resizable()
+                            .frame(width: 24, height: 24)
+                            .accessibility(label: Text(isFavorited ? "Remove from favorites" : "Add to favorites"))
+                    }
+                }
+        ).sheet(isPresented: $webViewShown) {
+            WebView(url: self.recipe.url)
+                .navigationBarTitle(Text(self.recipe.title), displayMode: .inline)
+                .embedInNavigation()
+        }
+    }
+}
---
+++ Recipes/Features/Details/RecipeDetailsView.swift
@@ -0,0 +1,56 @@
+//
+//  RecipeDetailsView.swift
+//  Recipes
+//
+//  Created by Majid Jabrayilov on 11/10/19.
+//  Copyright © 2019 Majid Jabrayilov. All rights reserved.
+//
+import KingfisherSwiftUI
+import SwiftUI
+
+struct RecipeDetailsView: View {
+    let recipe: Recipe
+
+    private var subheadline: some View {
+        Text("\(Int(recipe.calories)) kcal ")
+            +
+            Text("\(Int(recipe.totalWeight)) gr")
+    }
+
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 16) {
+                KFImage(recipe.image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(height: 300)
+                    .clipped()
+
+                VStack(alignment: .leading, spacing: 8) {
+                    VStack(alignment: .leading) {
+                        Text(recipe.title)
+                            .font(.title)
+                            .fixedSize(horizontal: false, vertical: true)
+
+                        subheadline
+                            .font(.subheadline)
+                            .fixedSize(horizontal: false, vertical: true)
+                    }
+
+                    ForEach(recipe.ingredients, id: \.self) { ingridient in
+                        Text("• \(ingridient)")
+                            .fixedSize(horizontal: false, vertical: true)
+                    }
+                }.padding(.horizontal)
+            }
+        }
+    }
+}
+
+struct RecipeDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeDetailsView(
+            recipe: .mock
+        )
+    }
+}
