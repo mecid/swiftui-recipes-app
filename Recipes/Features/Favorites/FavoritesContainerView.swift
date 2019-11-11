8e5ef2070915117eb9691e0b21390658bcd92dbe
Majid Jabrayilov <cmecid@gmail.com>
2019-11-11 13:45:41 +0400

    Favorites screen implemented.

---
+++ Recipes/Features/Favorites/FavoritesContainerView.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesContainerView.swift
+//  Recipes
+//
+//  Created by Majid Jabrayilov on 11/10/19.
+//  Copyright © 2019 Majid Jabrayilov. All rights reserved.
+//
+import SwiftUI
+
+struct FavoritesContainerView: View {
+    @EnvironmentObject var store: Store<AppState, AppAction>
+
+    var body: some View {
+        RecipesView(recipes: store.state.favorited)
+            .navigationBarTitle("Favorites")
+    }
+}
