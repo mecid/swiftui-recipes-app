199379351b8a9f0d6b334adc6a7d94b27d998ba9
Majid Jabrayilov <cmecid@gmail.com>
2019-11-14 03:08:56 +0400

    App logic refactored

--- Recipes/Shared/Model/App.swift
+++ Recipes/Shared/Model/App.swift
@@ -9,7 +9,7 @@
 struct AppState: Codable {
     var recipes: [Recipe] = []
     var favorited: [Recipe] = []
-    var isLoading = false
+    var health: Health = .gluten
     var nextPage = 0
 }
 
@@ -17,7 +17,7 @@ enum AppAction {
     case append(recipes: [Recipe], nextPage: Int)
     case saveToFavorites(recipe: Recipe)
     case removeFromFavorites(recipe: Recipe)
-    case set(loading: Bool)
+    case setHealth(health: Health)
     case resetState
 }
 
@@ -26,16 +26,14 @@ let appReducer: Reducer<AppState, AppAction> = Reducer { state, action in
     case let .append(recipes, nextPage):
         state.recipes.append(contentsOf: recipes)
         state.nextPage = nextPage
-        state.isLoading = false
     case let .saveToFavorites(recipe):
         state.favorited.append(recipe)
     case let .removeFromFavorites(recipe):
         state.favorited.removeAll { $0.url == recipe.url }
-    case let .set(loading):
-        state.isLoading = loading
     case .resetState:
         state.recipes.removeAll()
-        state.isLoading = false
         state.nextPage = 0
+    case .setHealth(let health):
+        state.health = health
     }
 }
