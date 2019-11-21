795abf1532d6772bfa48e66ad947166d7caf06a6
Majid Jabrayilov <cmecid@gmail.com>
2019-11-22 01:32:11 +0400

    Reader mode configuration added to SafariView

--- Recipes/Shared/Views/SafariView.swift
+++ Recipes/Shared/Views/SafariView.swift
@@ -12,9 +12,12 @@ struct SafariView: UIViewControllerRepresentable {
     typealias UIViewControllerType = SFSafariViewController
 
     let url: URL
+    let readerMode: Bool
 
     func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
-        SFSafariViewController(url: url)
+        let configuration = SFSafariViewController.Configuration()
+        configuration.entersReaderIfAvailable = readerMode
+        return SFSafariViewController(url: url, configuration: configuration)
     }
 
     func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {
