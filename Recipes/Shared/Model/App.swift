c83590ed58f36ddace63f60946c5473269558152
Majid Jabrayilov <cmecid@gmail.com>
2020-03-01 21:29:17 +0400

    App reducer refactored

--- Recipes/Shared/Model/App.swift
+++ Recipes/Shared/Model/App.swift
@@ -5,6 +5,19 @@
 //  Created by Majid Jabrayilov on 11/10/19.
 //  Copyright © 2019 Majid Jabrayilov. All rights reserved.
 //
+import Combine
+import Foundation
+
+// For more information check "How To Control The World" - Stephen Celis
+// https://vimeo.com/291588126
+struct World {
+    var counter = LaunchCounter()
+    var decoder = JSONDecoder()
+    var encoder = JSONEncoder()
+    var service: RecipesService = .live
+    var files = FileManager.default
+}
+
 struct AppState: Codable, Equatable {
     var allRecipes: [String: Recipe] = [:]
     var recipes: [String] = []
@@ -17,10 +30,18 @@ enum AppAction {
     case saveToFavorites(recipe: Recipe)
     case removeFromFavorites(recipe: Recipe)
     case setHealth(health: Health)
+    case search(query: String, page: Int = 0)
+    case set(state: AppState)
     case resetState
+    case load
+    case save
 }
 
-func appReducer(state: inout AppState, action: AppAction) {
+func appReducer(
+    state: inout AppState,
+    action: AppAction,
+    environment: World
+) -> AnyPublisher<AppAction, Never>? {
     switch action {
     case let .append(recipes):
         recipes.forEach { state.allRecipes[$0.uri] = $0 }
@@ -37,7 +58,31 @@ func appReducer(state: inout AppState, action: AppAction) {
     case .setHealth(let health):
         state.health = health
         state.recipes.removeAll()
+    case let .search(query, page):
+        return environment.service
+            .fetch(query, state.health, page)
+            .replaceError(with: [])
+            .map { .append(recipes: $0)}
+            .eraseToAnyPublisher()
+    case let .set(newState):
+        state = newState
+    case .load:
+        return environment.files
+            .read(name: "state.json", in: .applicationSupportDirectory)
+            .decode(type: AppState.self, decoder: environment.decoder)
+            .replaceError(with: AppState())
+            .map { AppAction.set(state: $0) }
+            .eraseToAnyPublisher()
+    case .save:
+        return Just(state)
+            .encode(encoder: environment.encoder)
+            .flatMap { environment.files.write(data: $0, name: "state.json", in: .applicationSupportDirectory) }
+            .map { _ in AppAction.resetState }
+            .replaceError(with: .resetState)
+            .eraseToAnyPublisher()
     }
+
+    return Empty().eraseToAnyPublisher()
 }
 
-typealias AppStore = Store<AppState, AppAction>
+typealias AppStore = Store<AppState, AppAction, World>
